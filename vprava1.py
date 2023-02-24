44a25fb0aef54d021caf777f64bd7ff186df04c8
m1xture <109725502+m1xture@users.noreply.github.com>
2023-02-24 13:17:53 +0200

    less

--- vprava1.py
+++ vprava1.py
@@ -1,7 +1,15 @@
-a = float(input( 'a=? ' ))
-b = float(input( 'b=? ' ))
+# a = float(input( 'a=? ' ))
+# b = float(input( 'b=? ' ))
 
-if abs (a/b-b/ (a-b) ) <0.001:
-    print( 'Так' )
-else:
-    print( 'Ні' )
+# if abs (a/b-b/ (a-b) ) <0.001:
+#     print( 'Так' )
+# else:
+#     print( 'Ні' )
+# n=int(input('n=?'))
+# s = 0
+# while n>0:
+#     d = n%10
+#     s = s+d
+#     n = n//10
+
+# print('s = ', s)
