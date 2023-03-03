9f3a9a74843d7ba8f92107d9046e5cd828c74a23
m1xture <109725502+m1xture@users.noreply.github.com>
2023-03-03 20:23:04 +0200

    l2

--- vprava1.py
+++ vprava1.py
@@ -5,6 +5,9 @@
 #     print( 'Так' )
 # else:
 #     print( 'Ні' )
+
+
+
 # n=int(input('n=?'))
 # s = 0
 # while n>0:
@@ -13,3 +16,19 @@
 #     n = n//10
 
 # print('s = ', s)
+
+
+
+for a in range(1, 10):
+    for b in range(0, 10):
+        if 10*a+b == a*b+31:
+            print(10*a+b)
+
+
+# k = int(input('k=?'))
+# sum = 0
+# for i in range(1,k//2+1):
+#     if k%i == 0:
+#         sum = sum+i
+# if sum == k:print(k,'-доскональне число')
+# else:print(k,'не є доскональним числом')
